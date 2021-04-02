f1f43fd20c653af440a71554b6fc515d9d2ca4ec
neo <452198757@qq.com>
2021-04-02 15:09:02 +0800

    save~

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class VpnUi(QWidget):
     def __init__(self) -> None:
         super().__init__()
         self.initUi()
+        self.vpn_enable = False
 
     def initUi(self):
 
@@ -34,36 +35,41 @@ class VpnUi(QWidget):
         disable_btn.clicked.connect(self.disable_vpn)
 
 
-        choose_file_btn = QPushButton("打开文件",self)
-        choose_file_btn.move(150,50)
-        choose_file_btn.resize(50,20)
+        # choose_file_btn = QPushButton("打开文件",self)
+        # choose_file_btn.move(150,50)
+        # choose_file_btn.resize(50,20)
 
-        choose_file_btn.clicked.connect(self.open_file_dialog)
+        # choose_file_btn.clicked.connect(self.open_file_dialog)
 
-        self.vpnfile = QLineEdit("选择文件",self)
-        self.vpnfile.setFocus()
-        self.vpnfile.move(10,50)
+        # self.vpnfile = QLineEdit("选择文件",self)
+        # self.vpnfile.setFocus()
+        # self.vpnfile.move(10,50)
 
-        switch_btn = QPushButton("更新metric",self)
-        switch_btn.move(220,50)
+        open_btn = QPushButton("开放相关IP",self)
+        open_btn.move(220,50)
+        open_btn.clicked.connect(self.route_add)
 
         self.show()
 
 
     def enable_vpn(self):
+        if self.vpn_enable:
+            return
+
         ## 启动VPN
         result = os.popen("rasdial vpn")
         print(result.read())
         ## 获取当前路由
-        vpn = self.get_vpn_gateway("vpn")
-        print(vpn)
+        self.vpn = self.get_vpn_gateway("vpn")
+        self.vpn_enable = True
 
         ## 加大vpn权重
-        cmd = "route change 0.0.0.0 mask 0.0.0.0 {} metric 1".format(vpn)
+        cmd = "route change 0.0.0.0 mask 0.0.0.0 {} metric 1".format(self.vpn)
         result = os.popen(cmd)
         print(result.read())
 
     def disable_vpn(self):
+        self.vpn_enable = False
         result = os.popen("rasdial vpn /disconnect")
         print(result.read())
 
@@ -84,6 +90,12 @@ class VpnUi(QWidget):
         default_path = "{}{}config".format(os.path.dirname(__file__),os.sep)
         data_path = QFileDialog.getOpenFileName(self,'Open File',default_path,'*.txt',options=options)
         self.vpnfile.setText(data_path[0])
+
+    def route_add(self):
+        cmd = "route add 182.254.0.0 mask 255.255.0.0 10.9.65.254 metric 1"
+        result = os.popen(cmd)
+        print(result.read())
+
 
     def get_vpn_gateway(self,net_name:str):
         '''
